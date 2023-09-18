de6769dfd2d686eb31fda047a82639ff722d6ecd
kurovakv <kurovakv@I301W33>
2023-09-18 09:48:11 +0500

    Добавьте файлы проекта.

---
+++ Лабораторные работы первая часть/Лабораторные работы первая часть.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
+
+int FirstProgram() {
+    //Конвертер валют из рублей в доллары
+    double rate{};
+    double sum{};
+    setlocale(LC_ALL, "Russian");
+    std::cout << " Введите курс обмена: ";
+    std::cin >> rate;
+    std::cout << " Введите сумму: ";
+    std::cin >> sum;
+    const double output{ sum / rate };
+    std::cout << sum << " рубли " << output << "$";
+    return 0;
+}
+
+int SecondProgram()
+{
+    //Перевод метров в км
+    const int m_per_km{ 1000 };
+    setlocale(LC_ALL, "Russian");
+    int meters{};
+    std::cout << " Введите число метров: ";
+    std::cin >> meters;
+    const int kilometers{ meters / m_per_km };
+    const int remaining_meters{ meters % m_per_km };
+    std::cout << meters << " метры = " << kilometers << " километры и " << remaining_meters << " метры ";
+    return 0;
+}
+
+//Показ текущей даты и времени
+std::string currentDateTime() {
+    setlocale(LC_ALL, "Russian");
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+    char buffer[128];
+    strftime(buffer, sizeof(buffer), "%m-%d-%Y %X", now);
+    return buffer;
+}
+
+int ThirdProgram()
+{
+    std::cout << " Текущая дата и время: " << currentDateTime() << std::endl;
+    return 0;
+}
+
+int main()
+{
+    FirstProgram();
+    SecondProgram();
+    ThirdProgram();
+    return 0;
+}
